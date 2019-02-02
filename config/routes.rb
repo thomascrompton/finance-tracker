0e65cdcd71020b42fa3c9e43f7c5eaa13cdf1147
thomascrompton <tcrom2000@gmail.com>
2019-02-02 07:55:02 -0500

    Add user and stock association plus views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     devise_for :users
     root 'welcome#index'
     get 'my_portfolio', to: 'users#my_portfolio'
-    get 'search_stocks', to: 'stocks#search' 
+    get 'search_stocks', to: 'stocks#search'
+    resources :user_stocks, only: [:create, :destroy]
 end
